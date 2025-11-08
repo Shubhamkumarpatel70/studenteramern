92f44f22b25310ff1d4a3d42ad30658e2943b729
Shubhamkumarpatel70 <your.email@example.com>
2025-11-08 20:18:33 +0530

    Change Router from BrowserRouter to HashRouter for better routing support

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import api from './config/api';
 import Navbar from "./components/Navbar";
 import Footer from './components/Footer';
